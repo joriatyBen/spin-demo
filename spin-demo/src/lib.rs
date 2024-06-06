d06fe9a0f92852f7c11cfcdd8515439319d34a55
benjamin.bajorat <benjamin.bajorat@mayflower.de>
2024-06-06 18:38:05 +0200

    correct time measurement

--- spin-demo/src/lib.rs
+++ spin-demo/src/lib.rs
@@ -45,7 +45,7 @@ async fn hello_world(req: Request) -> Result<Response, Box<dyn Error>> {
     match serde_json::from_slice::<Order>(req.body().as_ref()) {
         Ok(order) => {
             //let response_body = format!("New order: {:?}", order);
-            let result = handle_order(order, start_time).expect("HOLY FUCK");
+            let result = handle_order(order, start_time).expect("Just No!");
             let end_time: DateTime<Utc> = Utc::now();
             Ok(Response::builder()
                 .status(200)
@@ -55,7 +55,7 @@ async fn hello_world(req: Request) -> Result<Response, Box<dyn Error>> {
                 .header("Access-Control-Allow-Headers", "Content-Type")
                 //.body(response_body)
                 //.body(format!("{:?}", handle_order(order, start_time).unwrap()))
-                .body(format!("Execution time: {}, result: {:?}", start_time - end_time, result))
+                .body(format!("Execution time: {}, result: {:?}", end_time - start_time, result))
                 .build())
         }
         Err(e) => {
@@ -75,10 +75,11 @@ async fn hello_world(req: Request) -> Result<Response, Box<dyn Error>> {
 fn get_product_quantity(conn: &pg::Connection, article: &Cart) -> Result<i32> {
     println!("ARTICLE NAME: {}", article.name);
     let sql =
-        format!("SELECT quantity FROM products.\"products-details\" WHERE name = '{}' AND article_number = '{}'", article.name, article.id); // Welcome to SQL-Injection!!!
-    //let params = vec![ParameterValue::Str(article.name)]; // ParameterValue is not working - fix later
+        format!("SELECT quantity FROM products.\"products-details\" WHERE name = '{}' AND article_number = '{}'", article.name, article.id); // Welcome to SQL-Injection its cool, its fun for everybody!!!
+    //let params = vec![ParameterValue::Str(article.name)]; // ParameterValue is not working - fix later (maybe)
     let rowset = conn.query(&sql, &[])?;
 
+    // match the fuck out of a match
     match rowset.rows.first() {
         None => Ok(0),
         Some(row) => match row.first() {
@@ -95,7 +96,7 @@ fn handle_order(order: Order, start_time: DateTime<Utc>) -> Result<i32> {
     let mut current_quantity = 0;
 
     for article in &order.checkout {
-        current_quantity = get_product_quantity(&conn, article).expect("DID NOT EXPECT THAT") - article.quantity;
+        current_quantity = get_product_quantity(&conn, article).expect("Did not expect that!") - article.quantity;
         if current_quantity <= 0 {
             println!("Insufficient stock for product: {:?}", article.name)
         } else {
@@ -110,7 +111,8 @@ fn handle_order(order: Order, start_time: DateTime<Utc>) -> Result<i32> {
 }
 
 fn insert_customer_data(conn: &pg::Connection, order: Order, start_time: DateTime<Utc>) -> Result<i32> {
-    let sql = format!("INSERT INTO products.\"customers\" (name, email, phone, address, city, pin, last_ordered) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}') ON CONFLICT (name, email) DO UPDATE SET last_ordered = EXCLUDED.last_ordered",
+    let sql = format!("INSERT INTO products.\"customers\" (name, email, phone, address, city, pin, last_ordered) \
+    VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}') ON CONFLICT (name, email) DO UPDATE SET last_ordered = EXCLUDED.last_ordered",
                       order.customer.name,
                       order.customer.email,
                       order.customer.phone,
@@ -118,12 +120,13 @@ fn insert_customer_data(conn: &pg::Connection, order: Order, start_time: DateTim
                       order.customer.city,
                       order.customer.pin,
                       start_time
-    );
+    ); //langweilt mich komplett
     let sql_execute = conn.execute(&sql, &[])?;
     println!("Customer data added: {:?}", sql_execute);
     Ok(0)
 }
 
+// Muss ich noch machen
 // fn insert_order_data(conn: &pg::Connection, order: Order, start_time: DateTime<Utc>) {
 //     let sql = format!("INSERT INTO products.\"orders\" (name, email, phone, address, city, pin, last_ordered) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}') \
 //     ON DUPLICATE KEY UPDATE last_ordered = VALUES(last_ordered)",

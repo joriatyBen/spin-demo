36bb0823709097761661f0686588af445b534b47
benjamin.bajorat <benjamin.bajorat@mayflower.de>
2024-06-07 14:17:18 +0200

    add order insert

--- spin-demo/src/lib.rs
+++ spin-demo/src/lib.rs
@@ -4,6 +4,7 @@ use anyhow::{anyhow, Result};
 use std::error::Error;
 use std::collections::HashMap;
 use chrono::prelude::*;
+use serde_json::json;
 
 // defined in spin.toml
 const DB_URL_ENV: &str = "DB_URL";
@@ -41,7 +42,6 @@ async fn order_request(req: Request) -> Result<Response, Box<dyn Error>> {
     let mut ordered_products: HashMap<String, i32> = HashMap::new();
     match serde_json::from_slice::<Order>(req.body().as_ref()) {
         Ok(order) => {
-            //let response_body = format!("New order: {:?}", order);
             let result = handle_order(order, start_time, &mut ordered_products).expect("Handling order failed");
             let end_time: DateTime<Utc> = Utc::now();
             Ok(Response::builder()
@@ -50,7 +50,6 @@ async fn order_request(req: Request) -> Result<Response, Box<dyn Error>> {
                 .header("Access-Control-Allow-Origin", "http://localhost:5173")
                 .header("Access-Control-Allow-Methods", "POST")
                 .header("Access-Control-Allow-Headers", "Content-Type")
-                //.body(response_body)
                 .body(format!("Execution time: {}. Successfully ordered: {:?}", end_time - start_time, ordered_products))
                 .build())
         }
@@ -86,16 +85,17 @@ fn handle_order(
             let sql = format!("UPDATE products.\"products-details\" \
             SET quantity = '{}' WHERE name = '{}'", current_quantity, article.name);
             let sql_execute = conn.query(&sql, &[])?;
+
             println!("Product quantity updated: {:?}", sql_execute);
             ordered_products.insert(article.name.clone(), article.quantity);
         }
     }
-    insert_customer_data(&conn, order, start_time).expect("Customer data insert failed");
+    insert_customer_data(&conn, &order, start_time).expect("Customer data insert failed");
+    insert_order_data(&conn, &order, ordered_products, start_time).expect("Order data insert failed");
     Ok(0)
 }
 
 fn get_product_quantity(conn: &pg::Connection, article: &Cart) -> Result<i32> {
-    println!("ARTICLE NAME: {}", article.name);
     // Welcome to SQL-Injection its cool, its fun for everybody!!!
     let sql =
         format!("SELECT quantity FROM products.\"products-details\" \
@@ -109,14 +109,34 @@ fn get_product_quantity(conn: &pg::Connection, article: &Cart) -> Result<i32> {
         Some(row) => match row.first() {
             None => Ok(0),
             Some(pg::DbValue::Int32(i)) => Ok(*i),
-            Some(other) => Err(anyhow!("Unexpected error: {:?}", other)),
+            Some(other) => Err(anyhow!(
+                "Unexpected error while fetching article quantity from database: {:?}", other)
+            ),
+        },
+    }
+}
+
+fn get_customer_id(conn: &pg::Connection, customer: &Customer) -> Result<i32> {
+    let sql =
+        format!("SELECT id FROM products.\"customers\" \
+        WHERE name = '{}' AND email = '{}'", customer.name, customer.email);
+    let rowset = conn.query(&sql, &[])?;
+
+    match rowset.rows.first() {
+        None => Ok(0),
+        Some(row) => match row.first() {
+            None => Ok(0),
+            Some(pg::DbValue::Int32(i)) => Ok(*i),
+            Some(other) => Err(anyhow!(
+                "Unexpected error while fetching customer id from database: {:?}", other)
+            ),
         },
     }
 }
 
 fn insert_customer_data(
     conn: &pg::Connection,
-    order: Order,
+    order: &Order,
     start_time: DateTime<Utc>
 ) -> Result<i32> {
     let sql = format!("INSERT INTO products.\"customers\" \
@@ -132,22 +152,25 @@ fn insert_customer_data(
                       start_time
     );
     let sql_execute = conn.execute(&sql, &[])?;
-    println!("Customer data added: {:?}", sql_execute);
+
+    println!("Customer data stored: {:?}", sql_execute);
     Ok(0)
 }
 
-// Muss ich noch machen
-// fn insert_order_data(conn: &pg::Connection, order: Order, start_time: DateTime<Utc>) {
-//     let sql = format!("INSERT INTO products.\"orders\" (name, email, phone, address, city, pin, last_ordered) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}') \
-//     ON DUPLICATE KEY UPDATE last_ordered = VALUES(last_ordered)",
-//                       order.customer.name,
-//                       order.customer.email,
-//                       order.customer.phone,
-//                       order.customer.address,
-//                       order.customer.city,
-//                       order.customer.pin,
-//                       start_time
-//     );
-//     let sql_execute = conn.execute(&sql, &[])?;
-//     println!("CUSTOMER DATA ADDED: {:?}", sql_execute);
-//}
+fn insert_order_data(
+    conn: &pg::Connection,
+    order: &Order,
+    ordered_products: &mut HashMap<String, i32>,
+    start_time: DateTime<Utc>
+) -> Result<i32> {
+    let sql = format!("INSERT INTO products.\"orders\" \
+    (timestamp_order_request, product_sum, total_order, customer_id) VALUES ('{}', '{}', '{}', '{}')",
+                      start_time,
+                      serde_json::to_string(&ordered_products).unwrap(),
+                      order.order_total,
+                      get_customer_id(&conn, &order.customer).expect("some")
+    );
+    let sql_execute = conn.execute(&sql, &[])?;
+    println!("Order data stored: {:?}", sql_execute);
+    Ok(0)
+}
